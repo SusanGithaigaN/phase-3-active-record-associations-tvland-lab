058a4933385e793b192f18d06784a65860980535
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-02-24 07:18:20 +0300

    Completed assignment

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+    #   actor has many characters
+    has_many :characters
+    # many characters & shows through characters
+    has_many :shows, through: :characters
+
+
+    # full_name
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+    def list_roles
+        self.characters.map do |character|
+          "#{character.name} - #{character.show.name}"
+        end
+    end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,10 @@
 class Character < ActiveRecord::Base
-  
+    #   return actor instance
+    belongs_to :actor
+    # show instance
+    belongs_to :show
+    # say_that_thing_you_say
+    def say_that_thing_you_say
+        "#{self.name} always says: #{self.catchphrase}"
+    end
 end
